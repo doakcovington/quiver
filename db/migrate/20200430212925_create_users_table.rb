9c0fb91f8c16de0d37a27021e4f848bfdf44f482
doakcovington <doakcovington@protonmail.com>
2020-04-30 14:33:37 -0700

    Built out users table

---
+++ db/migrate/20200430212925_create_users_table.rb
@@ -0,0 +1,8 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string = :name
+      t.string = :email
+      t.string = :password_digest #encrypts the users password 
+  end
+end
