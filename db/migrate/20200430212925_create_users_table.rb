86ed4000c41a643016a6d5049c3935286b8635ae
doakcovington <doakcovington@protonmail.com>
2020-04-30 14:52:33 -0700

    Created ski class and skis table

--- db/migrate/20200430212925_create_users_table.rb
+++ db/migrate/20200430212925_create_users_table.rb
@@ -1,8 +1,10 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string = :name
-      t.string = :email
-      t.string = :password_digest #encrypts the users password 
+      t.string :name
+      t.string :email
+      t.string :password_digest #encrypts the users password
+      
+      t.timestamps null: false
   end
 end
---
+++ db/migrate/20200430214002_create_skis.rb
@@ -0,0 +1,13 @@
+class CreateSkis < ActiveRecord::Migration
+  def change
+    create_table :skis do |t|
+      t. string :name
+      t.string :brand
+      t.string :type
+      t.integer :height
+      t.integer :user_id
+      
+      t.timestamps null: false
+    end
+  end
+end
