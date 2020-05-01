043117072e947c5721f3a81fa7fef9dde5ae93df
doakcovington <doakcovington@protonmail.com>
2020-05-01 12:05:34 -0700

    Enable sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,24 @@ class UsersController < ApplicationController # => Gives class every method insi
     post '/login' do
         #sets @user equal to current email address
         #if returns 'nil' then user needs to signup
-        @user = User.find_by(email: params[:email])
-
-        if @user.authenticate(params[:email])
+        @user = User.find_by(email: params[:email]) # email: is the key (find_by requires key/value pair)
+        if @user.authenticate(params[:params])
             #redirect
+            session[:user_id] = @user.id #logs user in
+            redirect "/users/#{@user.id}"
         else
             #tell user info is invalid
             #redirect to login page
         end
-        binding.pry
     end
 
     #renders signup page
     get '/signup' do
 
+    end
+
+    get "/users/:id/" do
+        "Hello"
     end
 
 end
