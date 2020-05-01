123a27a9217a3e80882b5db6aad556985486202d
doakcovington <doakcovington@protonmail.com>
2020-04-30 18:57:32 -0700

    Created login and signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController # => Gives class every method inside ApplicationController
+
+    #renders login page
+    get '/login' do 
+        erb :login
+    end
+
+    #renders signup page
+    get '/signup' do
+
+    end
 
 end
