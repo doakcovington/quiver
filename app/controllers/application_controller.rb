52c54d143c7e30b4ef0b0139d7780d8241657d02
doakcovington <doakcovington@protonmail.com>
2020-05-01 09:17:02 -0700

    Added sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #allows for a cookie to presist across multiple HTTP requests 
+    set :session_secret, "prayforsnow" #allows for session id 
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,16 @@ class UsersController < ApplicationController # => Gives class every method insi
     end
 
     post '/login' do
+        #sets @user equal to current email address
+        #if returns 'nil' then user needs to signup
+        @user = User.find_by(email: params[:email])
+
+        if @user.authenticate(params[:email])
+            #redirect
+        else
+            #tell user info is invalid
+            #redirect to login page
+        end
         binding.pry
     end
 
