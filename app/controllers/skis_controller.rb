384519ea622c2dcd7043a45343fc213bb9945fd5
doakcovington <doakcovington@protonmail.com>
2020-05-04 11:16:01 -0700

    ski objects now have width attribute added or edited

--- app/controllers/skis_controller.rb
+++ app/controllers/skis_controller.rb
@@ -16,7 +16,7 @@ class SkisController < ApplicationController
             redirect '/login'
         end
         if params[:name] != "" && params[:brand] != "" && params[:category] != "" #checks if all text fields contain text
-            @ski = Ski.create(name: params[:name], brand: params[:brand], category: params[:category], user_id: @current_user.id)
+            @ski = Ski.create(name: params[:name], brand: params[:brand], width: params[:width], category: params[:category], user_id: @current_user.id)
             binding.pry
              redirect "/skis/#{@ski.id}"
         else
@@ -43,7 +43,7 @@ class SkisController < ApplicationController
         @ski = Ski.find(params[:id]) #finds the ski
         if logged_in?
             if @ski.user == current_user && params[:name] != "" && params[:brand] != "" && params[:category] != ""
-                @ski.update(name: params[:name], brand: params[:brand], category: params[:category])
+                @ski.update(name: params[:name], brand: params[:brand], width: params[:width], category: params[:category])
                 redirect "/skis/#{@ski.id}" #shows the ski
             else
                 redirect "/skis/#{@ski.id}/edit"
