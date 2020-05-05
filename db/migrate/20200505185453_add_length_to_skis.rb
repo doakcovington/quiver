1ed4aca6f8598bbed991359636761e8863c18c2d
doakcovington <doakcovington@protonmail.com>
2020-05-05 11:57:39 -0700

    Added length to ski database

---
+++ db/migrate/20200505185453_add_length_to_skis.rb
@@ -0,0 +1,7 @@
+class AddLengthToSkis < ActiveRecord::Migration
+  def change
+    def change
+      add_column :skis, :length, :integer
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200504175324) do
+ActiveRecord::Schema.define(version: 20200505185453) do
 
   create_table "skis", force: :cascade do |t|
     t.string   "name"
